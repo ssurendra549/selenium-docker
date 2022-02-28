1c88d72d8b7cc69847a4d98af9f05a9713e3ac02
ssurendra549 <73332632+sajjasurendra@users.noreply.github.com>
2022-02-28 23:24:54 +0530

    changes

---
+++ src/main/java/SeleniumDocker/Selenium_with_docker/App.java
@@ -0,0 +1,38 @@
+package SeleniumDocker.Selenium_with_docker;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+
+{
+	static WebDriver driver;
+	static final String APP_URL = "https://www.ibm.com";
+	static final String HOST_URL = "http://localhost:4444";
+	@Test
+	public static void getDriver() throws InterruptedException, MalformedURLException {
+		ChromeOptions opt = new ChromeOptions();
+	//	WebDriverManager.chromedriver().version("98.0.4758.102").setup();
+		//driver = new ChromeDriver();
+		driver = new RemoteWebDriver(new URL(HOST_URL), opt);
+		driver.get(APP_URL);
+		System.out.println("Title is " + driver.getTitle());
+		Thread.sleep(1000000);
+	driver.quit();
+}
+}
